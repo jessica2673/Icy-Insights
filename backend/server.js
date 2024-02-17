19350a9115c6ab0069481730fd881eff57e4f2be
Jessica <jessica2003mail@gmail.com>
2024-02-17 00:43:10 -0500

    setup database

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const keys = require('./config/keys.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/mern-stack-db', { useNewUrlParser: true, useUnifiedTopology: true });
-// Define routes and middleware
+mongoose.connect(keys.mongo.dbURI)
+  .then((result) => {
+      app.listen(4000);
+      console.log('connected to db');
+  })
+  .catch((err) => {
+      console.log(err);
+  });
+
+    // Define routes and middleware
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use('/test', () => {console.log('test works')});
