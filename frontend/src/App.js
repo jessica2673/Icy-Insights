1e8521f0368cfc08b1305052c7503467c3b0f817
evelina <91142355+135ze@users.noreply.github.com>
2024-02-17 03:21:11 -0500

    basic routing for home and signin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import Map from "./components/Map/Map";
+import SignIn from "./components/SignIn/SignIn";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Map/>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Navigate to="/home" replace />}
+          />
+          <Route path="/home" element={<Map />}/>
+          <Route path="/signin" element={<SignIn />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
