4e382c8f8dc7c9e333c8582e562544351ae97e33
provanni <provanni@sheridanc.on.ca>
2024-02-17 12:21:27 -0500

    theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
 import Map from "./components/Map/Map";
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp"
+import { ThemeProvider } from "@mui/material";
+import theme from './Theme'; // Import the theme
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<Navigate to="/home" replace />}
-          />
-          <Route path="/home" element={<Map />}/>
-          <Route path="/sign-in" element={<SignIn />}/>
-          <Route path="/sign-up" element={<SignUp />}/>
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={<Navigate to="/home" replace />}
+            />
+            <Route path="/home" element={<Map />}/>
+            <Route path="/sign-in" element={<SignIn />}/>
+            <Route path="/sign-up" element={<SignUp />}/>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
