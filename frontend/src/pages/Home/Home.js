f75ef1656baa66c8ebeab0c28f337389e03145f8
evelina <91142355+135ze@users.noreply.github.com>
2024-02-17 14:04:23 -0500

    bottom drawer in map page

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Map from '../../components/Map/Map'
+import Drawer from "../../components/Drawer/Drawer"
 
 const Home = () => {
   
   return (
     <div>
-
         <Map/>
+        <Drawer/>
     </div>
   );
 }
