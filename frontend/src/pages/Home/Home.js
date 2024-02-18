32e24e5271d91002dbc746074445a79b38a715d7
evelina <91142355+135ze@users.noreply.github.com>
2024-02-18 00:06:52 -0500

    add sign in avatar

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import Map from '../../components/Map/Map'
 import Drawer from "../../components/Drawer/Drawer"
+import { IconButton } from '@mui/material';
+import { AccountCircle } from '@mui/icons-material';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../../Theme';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const toSignIn = () => {
+    navigate('/sign-in');
+  };
   
   return (
-    <div>
-        <Map/>
-        <Drawer/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <IconButton
+        sx={{
+          position: 'absolute',
+          pointerEvents: 'auto',
+          top: 0,
+          right: 0,
+          margin: 3,
+          zIndex: 500,
+          width: '40px',
+          height: '40px',
+          borderRadius: '50%', 
+          backgroundColor: 'white', 
+        }}
+        onClick={toSignIn}
+      >
+        <AccountCircle fontSize="large" sx={{color: 'secondary.main'}} />
+      </IconButton>
+      <Map />
+      <Drawer />
+    </ThemeProvider>
   );
 }
-
 
 export default Home;
